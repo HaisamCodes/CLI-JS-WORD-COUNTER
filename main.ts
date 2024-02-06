f75dfc1e985b9999dd478d6031cb3f205a663f90
HaisamCodes <153128143+HaisamCodes@users.noreply.github.com>
2024-02-06 05:17:35 +0500

    Commit

---
+++ main.ts
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+async function wordCounter() {
+     const userInput = await inquirer.prompt([
+    {
+        name: "sentance",
+        type:"input",
+        message:"Enter your sentance to count the words"
+    }
+]); 
+    const formattedSentance = userInput.sentance.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/\s{2,}/g, " ").trim();
+
+    const wordCount = formattedSentance.split(" ").length;
+    
+    console.log(`The sentence has ${wordCount} ${wordCount === 1 ? 'word' : 'words'}.`);
+}
+wordCounter()
